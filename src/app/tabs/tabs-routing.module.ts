647f99c39d2004434e9291c1ab7240b471881f52
Administrator <Admin@Mac0.local>
2020-08-06 13:46:35 -0400

    fix the android tab header

--- src/app/tabs/tabs-routing.module.ts
+++ src/app/tabs/tabs-routing.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { TabsPage } from "./tabs.page";
-import { UserTasksPage } from "../Components/user-tasks/user-tasks.page";
-
 const routes: Routes = [
   {
     path: "",
@@ -32,6 +30,7 @@ const routes: Routes = [
         loadChildren: () =>
           import("../tab3/tab3.module").then((m) => m.Tab3PageModule),
       },
+      
       // {
       //   path: "usertasks",
       //   // component: UserTasksPage
@@ -48,7 +47,6 @@ const routes: Routes = [
   },
  
 ];
-
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule],
